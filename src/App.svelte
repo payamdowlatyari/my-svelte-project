92f34ee1219e0f8d8828d992cdf496be74ec8cd1
Payam Dowlatyari <pdowlatyari@gmail.com>
2021-08-22 12:17:45 -0700

    change bg and colors

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,14 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<nav>
+		<h1>I am {name}</h1>
+	</nav>
+
+	<p>
+		Visit my website at <a href="https://www.payamd.com">payamd.com</a> for more
+		information
+	</p>
 </main>
 
 <style>
@@ -13,13 +19,31 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		background-color: #222;
+		height: 100vh;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: chocolate;
+		opacity: 0.5;
 		font-size: 4em;
+		font-weight: 500;
+	}
+
+	p {
+		color: darkgrey;
 		font-weight: 100;
+	}
+
+	a {
+		color: #ddd;
+		font-weight: bolder;
+	}
+
+	a:hover {
+		text-decoration: none;
+		color: #fff;
+		transition: 0.5s;
 	}
 
 	@media (min-width: 640px) {
